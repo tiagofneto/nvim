5492b5939f21929dea98e9fdd7bfcd4bf902c48c
tiagofneto <tiagofneto29@gmail.com>
2023-05-16 07:50:05 +0700

    lualine styling

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -3,7 +3,9 @@ require('lualine').setup {
     icons_enabled = true,
     theme = 'auto',
     component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    --component_separators = '|',
+    --section_separators = { left = '', right = ''},
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = { 'NvimTree' },
       winbar = { 'NvimTree' },
@@ -18,12 +20,12 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'mode'},
+    lualine_a = {{ 'mode', separator = { left = '' }, right_padding = 2 }},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_z = {{ 'location', separator = { right = '' }, left_padding = 2 }}
   },
   inactive_sections = {
     lualine_a = {},
