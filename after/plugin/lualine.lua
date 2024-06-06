6cccd8513ada9db8e46420d1bcfe92c994cd628c
tiagofneto <tiagofneto29@gmail.com>
2024-06-06 08:34:11 +0100

    status line korean

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,3 +1,43 @@
+local mode_map = {
+  ['NORMAL'] = '일반',
+  ['O-PENDING'] = 'O-대기',
+  ['INSERT'] = '삽입',
+  ['VISUAL'] = '시각',
+  ['V-BLOCK'] = 'V-블록',
+  ['V-LINE'] = 'V-라인',
+  ['V-REPLACE'] = 'V-대체',
+  ['REPLACE'] = '대체',
+  ['COMMAND'] = '명령',
+  ['SHELL'] = '쉘',
+  ['TERMINAL'] = '터미널',
+  ['EX'] = 'EX',
+  ['S-BLOCK'] = 'S-블록',
+  ['S-LINE'] = 'S-라인',
+  ['SELECT'] = '선택',
+  ['CONFIRM'] = '확인?',
+  ['MORE'] = '더보기',
+}
+
+--local mode_map = {
+  --['NORMAL'] = '正常',
+  --['O-PENDING'] = 'O-待定',
+  --['INSERT'] = '插入',
+  --['VISUAL'] = '可视',
+  --['V-BLOCK'] = 'V-块',
+  --['V-LINE'] = 'V-行',
+  --['V-REPLACE'] = 'V-替换',
+  --['REPLACE'] = '替换',
+  --['COMMAND'] = '命令',
+  --['SHELL'] = '壳',
+  --['TERMINAL'] = '终端',
+  --['EX'] = 'EX',
+  --['S-BLOCK'] = 'S-块',
+  --['S-LINE'] = 'S-行',
+  --['SELECT'] = '选择',
+  --['CONFIRM'] = '确认?',
+  --['MORE'] = '更多',
+--}
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -20,7 +60,7 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {{ 'mode', separator = { left = '' }, right_padding = 2 }},
+    lualine_a = {{ 'mode', separator = { left = '' }, right_padding = 2, fmt = function(mode) return mode_map[mode] end }},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
