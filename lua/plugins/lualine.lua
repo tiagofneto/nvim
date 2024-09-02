942ce0326d50c17f7b334491f4186f60f6c6d67f
tiagofneto <tiagofneto29@gmail.com>
2024-09-02 20:00:28 +0100

    lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,37 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        local mode_map = {
+            ['NORMAL'] = '일반',
+            ['O-PENDING'] = 'O-대기',
+            ['INSERT'] = '삽입',
+            ['VISUAL'] = '시각',
+            ['V-BLOCK'] = 'V-블록',
+            ['V-LINE'] = 'V-라인',
+            ['V-REPLACE'] = 'V-대체',
+            ['REPLACE'] = '대체',
+            ['COMMAND'] = '명령',
+            ['SHELL'] = '쉘',
+            ['TERMINAL'] = '터미널',
+            ['EX'] = 'EX',
+            ['S-BLOCK'] = 'S-블록',
+            ['S-LINE'] = 'S-라인',
+            ['SELECT'] = '선택',
+            ['CONFIRM'] = '확인?',
+            ['MORE'] = '더보기',
+        }
+
+        require("lualine").setup({
+            options = {
+                disabled_filetypes = {
+                    statusline = { 'NvimTree' },
+                    winbar = { 'NvimTree' },
+                }
+            },
+            sections = {
+                lualine_a = {{ 'mode', separator = { left = '' }, right_padding = 2, fmt = function(mode) return mode_map[mode] end }},
+            }
+        })
+    end
+}
