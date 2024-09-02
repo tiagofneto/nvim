da7afd9a4a72be17195dfa752ae7a78ddb752fa1
tiagofneto <tiagofneto29@gmail.com>
2024-09-02 19:30:36 +0100

    telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+        require('telescope').load_extension('fzf')
+    end
+}
