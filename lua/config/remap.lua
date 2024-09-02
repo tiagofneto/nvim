0f998709c52c93446a3d478f4ef201af4ec7eb3d
tiagofneto <tiagofneto29@gmail.com>
2024-09-02 18:57:05 +0100

    nvim tree

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = ","
 
 -- navigate between splits
 vim.keymap.set('n', 'vv', '<C-w>v')
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local nvimtree = require("nvim-tree")
+
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvimtree.setup()
+
+    vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
+  end
+}
