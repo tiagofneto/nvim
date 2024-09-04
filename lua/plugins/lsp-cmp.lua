d778e029c4fe7bbc148b867fccde47075aa1e319
tiagofneto <tiagofneto29@gmail.com>
2024-09-04 19:21:01 +0100

    lint

--- lua/plugins/lsp-cmp.lua
+++ lua/plugins/lsp-cmp.lua
@@ -64,7 +64,7 @@ return {
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
