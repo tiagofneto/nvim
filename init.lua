cb7d09d567276bc3750f501a61ffb79d40cace09
Tiago Neto <tiagofneto29@gmail.com>
2025-08-19 09:19:01 +0200

    more stuff

--- init.lua
+++ init.lua
@@ -1,10 +1,21 @@
 --[[ TODOS
---Replace all :command
---Replace all vim.cmd
---Figure out icons for mini pick
+Replace all :command
+Replace all vim.cmd
+Figure out icons for mini pick
+Add dictionaries
+Add thesaurus
 ]]
 vim.o.number = true
 vim.o.relativenumber = true
+
+vim.o.termguicolors = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -24,16 +35,19 @@ vim.o.wrap = false
 
 vim.o.winborder = "rounded"
 
+vim.o.mouse = "a"
+
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :so<CR>')
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>')
+vim.keymap.set('n', '<leader>x', ':.lua<CR>')
+vim.keymap.set('v', '<leader>x', ':lua<CR>')
 
 vim.pack.add({
-	"https://github.com/navarasu/onedark.nvim",
-	"https://github.com/echasnovski/mini.pick",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/mason-org/mason.nvim"
+    "https://github.com/navarasu/onedark.nvim",
+    "https://github.com/echasnovski/mini.pick",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/mason-org/mason.nvim"
 })
 
 require "mini.pick".setup()
@@ -43,38 +57,50 @@ vim.keymap.set('n', '<leader><leader>', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 
 vim.lsp.enable({ "lua_ls" })
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true)
+            }
+        }
+    }
+})
 
 vim.cmd("colorscheme onedark")
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('my.lsp', {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		if client:supports_method('textDocument/implementation') then
-			-- Create a keymap for vim.lsp.buf.implementation ...
-		end
+    group = vim.api.nvim_create_augroup('custom.lsp', {}),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        -- if client:supports_method('textDocument/implementation') then
+        -- Create a keymap for vim.lsp.buf.implementation ...
+        -- end
 
-		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-		if client:supports_method('textDocument/completion') then
-			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
-			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-			client.server_capabilities.completionProvider.triggerCharacters = chars
+        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+        if client:supports_method('textDocument/completion') then
+            -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+            -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+            -- client.server_capabilities.completionProvider.triggerCharacters = chars
 
-			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-		end
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
 
-		-- Auto-format ("lint") on save.
-		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-		if not client:supports_method('textDocument/willSaveWaitUntil')
-		    and client:supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-				buffer = args.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-				end,
-			})
-		end
-	end,
+            vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+        end
+
+        -- Auto-format ("lint") on save.
+        -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+        if not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = vim.api.nvim_create_augroup('custom.lsp', { clear = false }),
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                end,
+            })
+        end
+    end,
 })
-vim.cmd("set completeopt+=noinsert")
+
+vim.lsp.omnifunc(1, 0)
