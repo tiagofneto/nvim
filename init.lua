5cdeed916e4d64c80c4c94b92e6c950c4ca6403b
Tiago Neto <tiagofneto29@gmail.com>
2025-08-28 12:39:33 +0200

    live grep

--- init.lua
+++ init.lua
@@ -61,6 +61,7 @@ require "mini.icons".setup()
 require "mason".setup()
 
 vim.keymap.set('n', '<leader><leader>', '<cmd>Pick files<CR>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Pick grep_live<CR>')
 vim.keymap.set('n', '<leader>h', '<cmd>Pick help<CR>')
 
 vim.diagnostic.config({
